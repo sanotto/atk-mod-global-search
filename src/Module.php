1c443ee878958523445ff537121563ba874f886b
Santiago Ottonello <sanotto@gmail.com>
2017-07-12 15:54:01 -0300

    Added session form

--- src/Module.php
+++ src/Module.php
@@ -12,9 +12,19 @@ class Module extends \Sintattica\Atk\Core\Module
     public function boot()
     {
     	//$this->registerNode('Users',Users::class,['admin', 'add', 'edit', 'delete', 'view']);
+		$sm = SessionManager::getInstance();
+        $sessionstatus = SessionManager::SESSION_DEFAULT,
+		$url=Tools::dispatch_url('Search.Search','search');
+        $script = 'atkSubmit("'.self::atkurlencode($sm->sessionUrl($url, $sessionstatus)).'")';
+		$text='<span class="glyphicon glyphicon-search"></span>&nbsp;'. Tools::atktext('search', $this->module);
 
-		$search_piece ='<input type="text" class="form-control" placeholder="'.Tools::atktext('searchfor', $this->module).'">';
-		$search_piece.='<button type="button" class="btn btn-info"> <span class="glyphicon glyphicon-search"></span>&nbsp;'. Tools::atktext('search', $this->module).' </button>';
+        $button = '<input type="button" class="btn btn-info"  value="'.$text.'" onClick=\''.$script.'\'>';
+		$search_piece = '';
+        $search_piece.= '<form name="entryform">';
+        $search_piece.= $sm->formState();
+		$search_piece.='<input type="text" class="form-control" placeholder="'.Tools::atktext('searchfor', $this->module).'">';
+        $search_piece.= $button;
+        $search_piece.= '</form>';
         $this->getMenu()->addMenuItem($search_piece, null, 'main', true, 0, static::$module, '', 'right');
 
     }
